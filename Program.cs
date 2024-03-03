4509454d5e013ce106a0200e1602c8c8b8ab8adc
duckkycats <earth_of_joke@hotmail.com>
2024-03-03 18:15:41 +0700

    - Add Exercise string and decimal dictionary

--- Program.cs
+++ Program.cs
@@ -110,6 +110,16 @@ namespace HelloWorld
 
             Console.WriteLine(myGroceryDictionary["Dairy"][2]);
 
+
+            //Exercise
+            Dictionary<string, decimal> itemPrices = new Dictionary<string, decimal>() {
+                {"cheese", 5.99m},
+                {"carrots", 2.99m}
+            };
+
+            Console.WriteLine(itemPrices["cheese"]);
+            Console.WriteLine(itemPrices["carrots"]);
+
         }
     }
 
--- obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+327454b0d1aee8a559e3873516b90ca0fbe3cb8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
