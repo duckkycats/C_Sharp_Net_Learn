23cc90afa53ae1e0e96bd46625dbdc801f3317cd
duckkycats <earth_of_joke@hotmail.com>
2024-03-31 13:48:18 +0700

    Learning Opeations

--- Program.cs
+++ Program.cs
@@ -1,124 +1,71 @@
 ﻿using System;
 
-namespace HelloWorld
+namespace HelloWorlds
 {
     internal class Program
     {
         static void Main(String[] arges)
         {
-            //// 1 byte is made up of 8 bits 00000000 - these bits can be used to store a number as follows
-            // //// Each bit can be worth 0 or 1 of the value it is placed in
-            // ////// From the right we start with a value of 1 and double for each digit to the left
-            // //// 00000000 = 0
-            // //// 00000001 = 1
-            // //// 00000010 = 2
-            // //// 00000011 = 3
-            // //// 00000100 = 4
-            // //// 00000101 = 5
-            // //// 00000110 = 6
-            // //// 00000111 = 7
-            // //// 00001000 = 8
- 
-            // 1 byte (8 bit) unsigned, where signed means it can be negative
-            // byte myByte = 255;
-            // byte mySecondByte = 0;
- 
-            // 1 byte (8 bit) signed, where signed means it can be negative
-            // sbyte mySbyte = 127;
-            // sbyte mySecondSbyte = -128;
- 
- 
-            // 2 byte (16 bit) unsigned, where signed means it can be negative
-            // ushort myUshort = 65535;
- 
-            // 2 byte (16 bit) signed, where signed means it can be negative
-            // short myShort = -32768;
- 
-            // 4 byte (32 bit) signed, where signed means it can be negative
-            // int myInt = 2147483647;
-            // int mySecondInt = -2147483648;
- 
-            // 8 byte (64 bit) signed, where signed means it can be negative
-            // long myLong = -9223372036854775808;
- 
- 
-            // 4 byte (32 bit) floating point number
-            // float myFloat = 0.751f;
-            // float mySecondFloat = 0.75f;
- 
-            // 8 byte (64 bit) floating point number
-            // double myDouble = 0.751;
-            // double mySecondDouble = 0.75d;
- 
-            // 16 byte (128 bit) floating point number
-            // decimal myDecimal = 0.751m;
-            // decimal mySecondDecimal = 0.75m;
- 
-            // Console.WriteLine(myFloat - mySecondFloat);
-            // Console.WriteLine(myDouble - mySecondDouble);
-            // Console.WriteLine(myDecimal - mySecondDecimal);
- 
-            // string myString = "Hello World";
+            // int myInt = 5;
+            // int mySecondInt = 10;
+
+            // Console.WriteLine(myInt.Equals(mySecondInt));
+            // Console.WriteLine(myInt.Equals(mySecondInt / 2));
+
+            // Console.WriteLine(myInt != mySecondInt);
+            // Console.WriteLine(myInt == mySecondInt / 2);
+            // Console.WriteLine(myInt != mySecondInt / 3);
+
+            // Console.WriteLine(myInt >= mySecondInt);
+            // Console.WriteLine(myInt >= mySecondInt / 2);
+            // Console.WriteLine(myInt > mySecondInt);
+            // Console.WriteLine(myInt > mySecondInt - 6);
+            // Console.WriteLine(myInt <= mySecondInt);
+            // Console.WriteLine(myInt < mySecondInt);
+
+            Console.WriteLine(5 > 10 && 5 < 10);
+
+            // myInt++;
+            // Console.WriteLine(myInt);
+
+            // myInt += 7;
+            // Console.WriteLine(myInt);
+
+            // myInt -= 8;
+            // Console.WriteLine(myInt);
+            
+            // Console.WriteLine(myInt * mySecondInt);
+
+            // Console.WriteLine(mySecondInt / myInt);
+
+            // Console.WriteLine(mySecondInt + myInt);
+
+            // Console.WriteLine(myInt - mySecondInt);
+
+            // Console.WriteLine(5 + 5 * 10);
+
+            // Console.WriteLine(Math.Pow(5,4));
+
+            // Console.WriteLine(Math.Sqrt(25));
+
+            // string myString = "test";
+
             // Console.WriteLine(myString);
-            // string myStringWithSymbols = "!@#$@^$%%^&(&%^*__)+%^@##$!@%123589071340698ughedfaoig137";
-            // Console.WriteLine(myStringWithSymbols);
- 
-            // bool myBool = true;
 
-            string[] myGroceryArray = new string[2];
+            // myString += ". second part.";
 
-            myGroceryArray[0] = "Guacamole";
-            // myGroceryArray[1] = "Nong Best";
+            // Console.WriteLine(myString);
+    
+            // myString = myString += " \"thrid\\ part.";
 
-            Console.WriteLine(myGroceryArray[0]);
-            Console.WriteLine(myGroceryArray[1]);
-            // Console.WriteLine(myGroceryArray[2]);
+            // Console.WriteLine(myString);
 
-            string[] mySecondGroceryArray = {"Apple", "Eggs"};
-
-            // mySecondGroceryArray[2] = "";
-
-            // Console.WriteLine(mySecondGroceryArray[0]);
-            // Console.WriteLine(mySecondGroceryArray[1]);
-            // Console.WriteLine(mySecondGroceryArray[2]);
-
-            List<string> myGroceryList = new List<string>() { "Milk", "Cheese" };
-
-            // Console.WriteLine(myGroceryList[0]);
-            // Console.WriteLine(myGroceryList[1]);
-
-            // Console.WriteLine(myGroceryList[2]); //Error
-
-            myGroceryList.Add("Oranges");
-
-            // Console.WriteLine(myGroceryList[2]);
-
-            IEnumerable<string> myGroceryIEnumerable = myGroceryList;
-
-            // Console.WriteLine(myGroceryIEnumerable.First());
-
-            string[,] myTwoDimensionalArray = new string[,] {
-                {"Apple", "Eggs"},
-                {"Milk", "Cheese"}
-            };
-
-            // Console.WriteLine(myTwoDimensionalArray[1,1]);
-
-            Dictionary<string, string[]> myGroceryDictionary = new Dictionary<string, string[]>() {
-                {"Dairy", new string[]{"Cheese", "Milk", "Eggs"}}
-            };
-
-            Console.WriteLine(myGroceryDictionary["Dairy"][2]);
+            // string[] myStringArr = myString.Split(". ");
+            // Console.WriteLine(myStringArr[0]);
+            // Console.WriteLine(myStringArr[1]);
 
 
-            //Exercise
-            Dictionary<string, decimal> itemPrices = new Dictionary<string, decimal>() {
-                {"cheese", 5.99m},
-                {"carrots", 2.99m}
-            };
 
-            Console.WriteLine(itemPrices["cheese"]);
-            Console.WriteLine(itemPrices["carrots"]);
 
         }
     }
