050f1402c8500be08e9648606adffec7c4e16f90
duckkycats <earth_of_joke@hotmail.com>
2024-05-19 17:39:06 +0700

    Backup File Program07 Learn about JSON

--- obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95256aedfa4b5e8ed1b72f46a0a4b397d5a6d0be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a52551b4ef8824c6b6b0a139fb4cf565c3bd934")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
