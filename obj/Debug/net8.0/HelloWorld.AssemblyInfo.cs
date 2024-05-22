6fa75a5046163334212cfd7d39ea4f0820dd2dba
duckkycats <earth_of_joke@hotmail.com>
2024-05-22 10:30:37 +0700

    add backup_08

--- obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+050f1402c8500be08e9648606adffec7c4e16f90")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cfeb61b5a20c0188f5b8404c0536635581dc377")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
